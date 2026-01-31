3091ee6dfebd0d92a96979903269b1ddca67d80b
giahuy1675 <giahuy752004@gmail.com>
2026-01-31 10:52:48 +0700

    Buoi2

---
+++ NguyenGiaHuy_Buoi2/src/main/java/com/example/nguyengiahuy_buoi2/NguyenGiaHuyBuoi2Application.java
@@ -0,0 +1,13 @@
+package com.example.nguyengiahuy_buoi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenGiaHuyBuoi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NguyenGiaHuyBuoi2Application.class, args);
+    }
+
+}
